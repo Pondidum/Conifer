c9f1723eb16f8f7e86c50e6c9b2c2ccf253ca684
andy <andy@andydote.co.uk>
2015-05-02 18:43:49 +0100

    refactor: extract interface for conventions

--- TypedRoutingTest/ConventionalRouter.cs
+++ TypedRoutingTest/ConventionalRouter.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+using System.Security.Cryptography.X509Certificates;
 using System.Web.Http;
 using System.Web.Http.Controllers;
 
@@ -20,24 +21,11 @@ namespace TypedRoutingTest
 			where TController : IHttpController
 		{
 
-			var conventions = new List<Action<RouteTemplate>>
+			var conventions = new List<IRouteConvetion>
 			{
-				rt =>
-				{
-					rt.Parts.Insert(0, prefix.TrimEnd('/'));
-				}, 
-				rt =>
-				{
-					var parameterNames = rt.Method.GetParameters().Select(p => "{" + p.Name + "}");
-					rt.Parts.Add(string.Join("/", parameterNames));
-				},
-				rt =>
-				{
-					if (rt.Method.Name.EndsWith("Raw"))
-					{
-						rt.Parts.Add("raw");
-					}
-				}
+				new PrefixRouteConvention(prefix),
+				new ParameterNameRouteConvention(),
+				new RawOptionRouteConvention()
 			};
 
 			var type = typeof(TController);
@@ -49,7 +37,7 @@ namespace TypedRoutingTest
 			foreach (var method in methods)
 			{
 				var rt = new RouteTemplate(method);
-				conventions.ForEach(convention => convention(rt));
+				conventions.ForEach(convention => convention.Execute(rt));
 
 				var template = string.Join("/", rt.Parts.Select(p => p.Trim('/')));
 
@@ -58,6 +46,46 @@ namespace TypedRoutingTest
 				route.Controller<TController>();
 
 				_configuration.TypedRoute(route);
+			}
+		}
+	}
+
+	public interface IRouteConvetion
+	{
+		void Execute(RouteTemplate template);
+	}
+
+	public class ParameterNameRouteConvention : IRouteConvetion
+	{
+		public void Execute(RouteTemplate template)
+		{
+			var parameterNames = template.Method.GetParameters().Select(p => "{" + p.Name + "}");
+			template.Parts.Add(string.Join("/", parameterNames));
+		}
+	}
+
+	public class PrefixRouteConvention : IRouteConvetion
+	{
+		private readonly string _prefix;
+
+		public PrefixRouteConvention(string prefix)
+		{
+			_prefix = prefix.TrimEnd('/');
+		}
+
+		public void Execute(RouteTemplate template)
+		{
+			template.Parts.Insert(0, _prefix);
+		}
+	}
+
+	public class RawOptionRouteConvention : IRouteConvetion
+	{
+		public void Execute(RouteTemplate template)
+		{
+			if (template.Method.Name.EndsWith("Raw"))
+			{
+				template.Parts.Add("raw");
 			}
 		}
 	}
