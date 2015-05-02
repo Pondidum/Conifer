688a3b14e0b9681924b595392974d3f8928f8f01
andy <andy@andydote.co.uk>
2015-05-02 18:37:35 +0100

    conventional: move each action into a convention list

--- TypedRoutingTest/ConventionalRouter.cs
+++ TypedRoutingTest/ConventionalRouter.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Web.Http;
@@ -18,24 +19,39 @@ namespace TypedRoutingTest
 		public void AddRoutes<TController>(string prefix)
 			where TController : IHttpController
 		{
+
+			var conventions = new List<Action<RouteTemplate>>
+			{
+				rt =>
+				{
+					rt.Parts.Insert(0, prefix.TrimEnd('/'));
+				}, 
+				rt =>
+				{
+					var parameterNames = rt.Method.GetParameters().Select(p => "{" + p.Name + "}");
+					rt.Parts.Add(string.Join("/", parameterNames));
+				},
+				rt =>
+				{
+					if (rt.Method.Name.EndsWith("Raw"))
+					{
+						rt.Parts.Add("raw");
+					}
+				}
+			};
+
 			var type = typeof(TController);
 			var methods = type
 				.GetMethods(BindingFlags.DeclaredOnly | BindingFlags.Instance | BindingFlags.Public)
 				.Where(m => m.ReturnType != typeof(void))
 				.ToList();
 
-			prefix = prefix.TrimEnd('/');
-
 			foreach (var method in methods)
 			{
-				//make into a convention
-				var parameterNames = method.GetParameters().Select(p => "{" + p.Name + "}");
+				var rt = new RouteTemplate(method);
+				conventions.ForEach(convention => convention(rt));
 
-				var raw = method.Name.EndsWith("raw", StringComparison.OrdinalIgnoreCase)
-					? "raw"
-					: "";
-
-				var template = string.Format("{0}/{1}/{2}", prefix, string.Join("/", parameterNames), raw);
+				var template = string.Join("/", rt.Parts.Select(p => p.Trim('/')));
 
 				var route = new TypedRoute(template);
 				route.Action(method.Name);
@@ -43,6 +59,18 @@ namespace TypedRoutingTest
 
 				_configuration.TypedRoute(route);
 			}
-		} 
+		}
+
+		private class RouteTemplate
+		{
+			public MethodInfo Method { get; private set; }
+			public List<string> Parts { get; private set; }
+
+			public RouteTemplate(MethodInfo method)
+			{
+				Method = method;
+				Parts = new List<string>();
+			}
+		}
 	}
 }
