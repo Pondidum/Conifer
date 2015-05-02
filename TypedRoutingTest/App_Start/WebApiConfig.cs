4679c32a993760f29ebdcce6e63a7515ae28796f
andy <andy@andydote.co.uk>
2015-05-02 17:49:59 +0100

    implement example deep parametered routing

--- TypedRoutingTest/App_Start/WebApiConfig.cs
+++ TypedRoutingTest/App_Start/WebApiConfig.cs
@@ -10,6 +10,25 @@ namespace TypedRoutingTest
 			config.MapHttpAttributeRoutes(new TypedDirectRouteProvider());
 
 			config.TypedRoute("", c => c.Action<HomeController>(h => h.Get()));
+
+
+			config.TypedRoute(
+				"candidate/ref/{refnum:int}",
+				c => c.Action<CandidateController>(h => h.GetRef(Arg.Any<int>())));
+
+			config.TypedRoute(
+				"candidate/ref/{refnum:int}/{folder}",
+				c => c.Action<CandidateController>(h => h.GetRefFolder(Arg.Any<int>(), Arg.Any<string>())));
+
+			config.TypedRoute(
+				"candidate/ref/{refnum:int}/{folder}/{file}",
+				c => c.Action<CandidateController>(h => h.GetRefFile(Arg.Any<int>(), Arg.Any<string>(), Arg.Any<string>())));
+
+			config.TypedRoute(
+				"candidate/ref/{refnum:int}/{folder}/{file}/raw",
+				c => c.Action<CandidateController>(h => h.GetRefFileRaw(Arg.Any<int>(), Arg.Any<string>(), Arg.Any<string>())));
+
+
 		}
 	}
 }
---
+++ TypedRoutingTest/Controllers/CandidateController.cs
@@ -0,0 +1,33 @@
+﻿using System.Net.Http;
+using System.Web.Http;
+
+namespace TypedRoutingTest.Controllers
+{
+	public class CandidateController : ApiController
+	{
+		//  candidate/ref/{refnum} => List<FileModel>
+		//  candidate/ref/{refnum}/{folder} => List<FileModel>
+		//  candidate/ref/{refnum}/{folder}/{file} => FileModel
+		//  candidate/ref/{refnum}/{folder}/{file}/raw => binary
+
+		public string GetRef(int refnum)
+		{
+			return "candidate/ref/{refnum}";
+		}
+
+		public string GetRefFolder(int refnum, string folder)
+		{
+			return "candidate/ref/{refnum}/{folder}";
+		}
+
+		public string GetRefFile(int refnum, string folder, string file)
+		{
+			return "candidate/ref/{refnum}/{folder}/{file}";
+		}
+
+		public string GetRefFileRaw(int refnum, string folder, string file)
+		{
+			return "candidate/ref/{refnum}/{folder}/{file}/raw";
+		}
+	}
+}
