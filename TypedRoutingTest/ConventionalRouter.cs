0f3bfcfbb88d8ca5c2bce6837aa5353124a84296
andy <andy@andydote.co.uk>
2015-05-04 18:23:48 +0100

    conventionalRouter: add methods for generating strong typed links

--- TypedRoutingTest/ConventionalRouter.cs
+++ TypedRoutingTest/ConventionalRouter.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Reflection;
 using System.Web.Http;
 using System.Web.Http.Controllers;
@@ -9,10 +11,12 @@ namespace TypedRoutingTest
 	public class ConventionalRouter
 	{
 		private readonly HttpConfiguration _configuration;
+		private readonly List<TypedRoute> _allRoutes;
 
 		public ConventionalRouter(HttpConfiguration configuration)
 		{
 			_configuration = configuration;
+			_allRoutes = new List<TypedRoute>();
 		}
 
 		public void AddRoutes<TController>(List<IRouteConvetion> conventions) where TController : IHttpController
@@ -32,8 +36,63 @@ namespace TypedRoutingTest
 				route.Action(method.Name);
 				route.Controller<TController>();
 
+				_allRoutes.Add(route);
 				_configuration.TypedRoute(route);
 			}
+		}
+
+		public string LinkTo<T>(Expression<Action<T>> expression)
+		{
+			var info = GetMethodInfo(expression);
+
+			var routes = _allRoutes
+				.Where(r => r.ControllerType == info.Class)
+				.Where(r => r.ActionName == info.Method.Name);
+
+			var template = routes.First().Template;
+
+			foreach (var pair in info.Parameters)
+			{
+				template = template.Replace("{" + pair.Key + "}", Convert.ToString(pair.Value));
+			}
+
+			return template;
+		}
+
+		private class MethodModel
+		{
+			public Type Class { get; set; }
+			public MethodInfo Method { get; set; }
+			public Dictionary<string, object> Parameters { get; set; }
+		}
+
+		private static MethodModel GetMethodInfoInternal(dynamic expression)
+		{
+			var method = expression.Body as MethodCallExpression;
+			if (method == null) throw new ArgumentException("Expression is incorrect!");
+
+			var parameters = method.Method.GetParameters();
+			var values = method.Arguments.Select(a => Expression.Lambda(a).Compile().DynamicInvoke()).ToList();
+
+			return new MethodModel
+			{
+				Method = method.Method,
+				Parameters = parameters
+					.Select((p, i) => new
+					{
+						Name = p.Name,
+						Value = values[i]
+					})
+					.ToDictionary(p => p.Name, p => p.Value)
+			};
+		}
+
+		private MethodModel GetMethodInfo<T>(Expression<Action<T>> expression)
+		{
+			var info = GetMethodInfoInternal(expression);
+			info.Class = typeof(T);
+
+			return info;
 		}
 	}
 }
