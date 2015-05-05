e4188859d957c9b0978b85dc70297962276791c0
andy <andy@andydote.co.uk>
2015-05-05 19:55:09 +0100

    refactor: extract method for building routes for controllers

--- TypedRoutingTest/ConventionalRouter.cs
+++ TypedRoutingTest/ConventionalRouter.cs
@@ -10,13 +10,14 @@ namespace TypedRoutingTest
 {
 	public class ConventionalRouter
 	{
+		public List<TypedRoute> AllRoutes { get; private set; }
+
 		private readonly HttpConfiguration _configuration;
-		private readonly List<TypedRoute> _allRoutes;
 
 		public ConventionalRouter(HttpConfiguration configuration)
 		{
 			_configuration = configuration;
-			_allRoutes = new List<TypedRoute>();
+			AllRoutes = new List<TypedRoute>();
 		}
 
 		public void AddRoutes<TController>(List<IRouteConvetion> conventions) where TController : IHttpController
@@ -36,27 +37,9 @@ namespace TypedRoutingTest
 				route.Action(method.Name);
 				route.Controller<TController>();
 
-				_allRoutes.Add(route);
+				AllRoutes.Add(route);
 				_configuration.TypedRoute(route);
 			}
-		}
-
-		public string LinkTo<T>(Expression<Action<T>> expression)
-		{
-			var info = MethodBuilder.GetMethodInfo(expression);
-
-			var routes = _allRoutes
-				.Where(r => r.ControllerType == info.Class)
-				.Where(r => r.ActionName == info.Method.Name);
-
-			var template = routes.First().Template;
-
-			foreach (var pair in info.Parameters)
-			{
-				template = template.Replace("{" + pair.Key + "}", Convert.ToString(pair.Value));
-			}
-
-			return template;
 		}
 	}
 }
---
+++ TypedRoutingTest/RouteBuilder.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace TypedRoutingTest
+{
+	public class RouteBuilder
+	{
+		private readonly List<TypedRoute> _routes;
+
+		public RouteBuilder(List<TypedRoute> routes)
+		{
+			_routes = routes;
+		}
+
+		public string RouteFor<T>(Expression<Action<T>> expression)
+		{
+			var info = MethodBuilder.GetMethodInfo(expression);
+
+			var routes = _routes
+				.Where(r => r.ControllerType == info.Class)
+				.Where(r => r.ActionName == info.Method.Name);
+
+			var template = routes.First().Template;
+
+			foreach (var pair in info.Parameters)
+			{
+				template = template.Replace("{" + pair.Key + "}", Convert.ToString(pair.Value));
+			}
+
+			return template;
+		}
+	}
+}
