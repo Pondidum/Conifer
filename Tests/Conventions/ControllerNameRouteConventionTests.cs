71eab09a73fe821d81454d7ba7d5a503519a0f3f
andy <andy@andydote.co.uk>
2015-05-08 18:46:34 +0100

    tests: add controllerNameRouteConvention Tests

---
+++ Tests/Conventions/ControllerNameRouteConventionTests.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using RestRouter;
+using RestRouter.Conventions;
+using Shouldly;
+using Xunit;
+
+namespace Tests.Conventions
+{
+	public class ControllerNameRouteConventionTests
+	{
+		private List<string> RunTest<T>()
+		{
+			var method = GetType().GetMethod("ToString");
+
+			var template = new RouteTemplateBuilder(typeof(T), method);
+
+			var convention = new ControllerNameRouteConvention();
+			convention.Execute(template);
+
+			return template.Parts;
+		}
+
+		[Fact]
+		public void When_a_controller_is_suffixed_with_controller()
+		{
+			RunTest<SuffixedController>().Single().ShouldBe("Suffixed");
+		}
+
+		[Fact]
+		public void When_a_controller_is_suffixed_twice()
+		{
+			RunTest<TestControllerController>().Single().ShouldBe("TestController");
+		}
+
+		[Fact]
+		public void When_a_controller_is_not_suffixed()
+		{
+			RunTest<NoSuffix>().Single().ShouldBe("NoSuffix");
+		}
+
+		internal class SuffixedController { }
+		internal class TestControllerController { }
+		internal class NoSuffix { }
+	}
+}
--- Tests/Scratchpad.cs
+++ Tests/Scratchpad.cs
@@ -3,7 +3,6 @@ using System.Web.Http;
 using RestRouter;
 using RestRouter.Conventions;
 using Shouldly;
-using TypedRoutingTest.Controllers;
 using Xunit;
 
 namespace Tests
@@ -13,23 +12,23 @@ namespace Tests
 		[Fact]
 		public void When_testing_something()
 		{
-			var conventions = new List<IRouteConvetion>
-			{
-				new ControllerNameRouteConvention(),
-				new SpecifiedPartRouteConvention("ref"),
-				new ParameterNameRouteConvention(),
-				new RawOptionRouteConvention()
-			};
+			//var conventions = new List<IRouteConvetion>
+			//{
+			//	new ControllerNameRouteConvention(),
+			//	new SpecifiedPartRouteConvention("ref"),
+			//	new ParameterNameRouteConvention(),
+			//	new RawOptionRouteConvention()
+			//};
 
-			var config = new HttpConfiguration();
-			var router = new ConventionalRouter(config);
+			//var config = new HttpConfiguration();
+			//var router = new ConventionalRouter(config);
 
-			router.AddRoutes<CandidateController>(conventions);
+			//router.AddRoutes<CandidateController>(conventions);
 
-			var builder = new RouteBuilder(router.Routes);
-			var route = builder.RouteFor<CandidateController>(c => c.GetRefFileRaw(GetRef(), "cvs", "file.docx"));
+			//var builder = new RouteBuilder(router.Routes);
+			//var route = builder.RouteFor<CandidateController>(c => c.GetRefFileRaw(GetRef(), "cvs", "file.docx"));
 
-			route.ShouldBe("candidate/ref/456/cvs/file.docx/raw", Case.Insensitive);
+			//route.ShouldBe("candidate/ref/456/cvs/file.docx/raw", Case.Insensitive);
 		}
 
 		private int GetRef()
