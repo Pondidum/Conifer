a0d228410e8574600df0f201d9b13b0073ad66b8
andy <andy@andydote.co.uk>
2015-05-09 18:03:33 +0100

    restRouter: add configuration express and httpConfiguration extension

---
+++ RestRouter/HttpConfigurationExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Web.Http;
+
+namespace RestRouter
+{
+	public static class HttpConfigurationExtensions
+	{
+		public static void AddConventionalRoutes(this HttpConfiguration self, Action<RouterConfigurationExpression> configure)
+		{
+			var router = new TypedDirectRouteProvider();
+			var expression = new RouterConfigurationExpression(router);
+			configure(expression);
+
+			self.MapHttpAttributeRoutes(router);
+		}
+	}
+}
---
+++ RestRouter/RouterConfigurationExpression.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http.Controllers;
+
+namespace RestRouter
+{
+	public class RouterConfigurationExpression
+	{
+		private readonly List<IRouteConvention> _defaultConventions;
+		private readonly ConventionalRouter _router;
+
+		public RouterConfigurationExpression(TypedDirectRouteProvider routeProvider)
+		{
+			_defaultConventions = new List<IRouteConvention>();
+			_router = new ConventionalRouter(routeProvider);
+		}
+
+		public void DefaultConventionsAre(IEnumerable<IRouteConvention> conventions)
+		{
+			_defaultConventions.Clear();
+			_defaultConventions.AddRange(conventions);
+		}
+
+		public void Add<TController>() where TController : IHttpController
+		{
+			Add<TController>(_defaultConventions);
+		}
+
+		public void Add<TController>(IEnumerable<IRouteConvention> conventions) where TController : IHttpController
+		{
+			if (conventions == null) conventions = Enumerable.Empty<IRouteConvention>();
+
+			_router.AddRoutes<TController>(conventions.ToList());
+		} 
+	}
+}
