a7bc12635a75b9aa9c3bb2fde9c5df43076dbe9f
andy <andy@andydote.co.uk>
2015-05-09 18:29:04 +0100

    tests: add routerConfigurationExpression tests

---
+++ Tests/RouterConfigurationExpressionTests.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Http.Controllers;
+using NSubstitute;
+using RestRouter;
+using RestRouter.Conventions;
+using Shouldly;
+using Xunit;
+
+namespace Tests
+{
+	public class RouterConfigurationExpressionTests
+	{
+		private readonly IConventionalRouter _router;
+		private readonly RouterConfigurationExpression _expression;
+
+		public RouterConfigurationExpressionTests()
+		{
+			_router = Substitute.For<IConventionalRouter>();
+			_expression = new RouterConfigurationExpression(_router);
+		}
+
+		[Fact]
+		public void When_no_default_conventions_are_specified()
+		{
+			_expression.Add<Controller>();
+
+			_router.Received().AddRoutes<Controller>(Arg.Do<List<IRouteConvention>>(x => x.ShouldBe(Default.Conventions)));
+		}
+
+		[Fact]
+		public void When_no_default_conventions_are_specified_and_individual_are_used()
+		{
+			_expression.Add<Controller>(new[] { new ControllerNameRouteConvention() });
+
+			_router.Received().AddRoutes<Controller>(Arg.Do<List<IRouteConvention>>(x => x.ShouldBe(new[] { new ControllerNameRouteConvention() })));
+		}
+
+		[Fact]
+		public void When_default_conventions_are_specified()
+		{
+			_expression.DefaultConventionsAre(new[] { new ControllerNameRouteConvention() });
+			_expression.Add<Controller>();
+
+			_router.Received().AddRoutes<Controller>(Arg.Do<List<IRouteConvention>>(x => x.ShouldBe(new[] { new ControllerNameRouteConvention() })));
+		}
+
+		private class Controller : IHttpController
+		{
+			public Task<HttpResponseMessage> ExecuteAsync(HttpControllerContext controllerContext, CancellationToken cancellationToken)
+			{
+				throw new System.NotImplementedException();
+			}
+		}
+	}
+}
