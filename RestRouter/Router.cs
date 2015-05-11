d4b9b30b78e757b012490b79240dcae92ce9c5a4
andy <andy@andydote.co.uk>
2015-05-11 19:41:24 +0100

    router: works better off as its own class rather than an extension method

--- RestRouter/Router.cs
+++ RestRouter/Router.cs
@@ -3,16 +3,18 @@ using System.Web.Http;
 
 namespace RestRouter
 {
-	public static class HttpConfigurationExtensions
+	public class Router
 	{
-		public static void AddConventionalRoutes(this HttpConfiguration self, Action<RouterConfigurationExpression> configure)
+		public static RouteBuilder Create(HttpConfiguration httpConfiguration, Action<RouterConfigurationExpression> configure)
 		{
 			var routeProvider = new TypedDirectRouteProvider();
 			var router = new ConventionalRouter(routeProvider);
 			var expression = new RouterConfigurationExpression(router);
 			configure(expression);
 
-			self.MapHttpAttributeRoutes(routeProvider);
-		}
+			httpConfiguration.MapHttpAttributeRoutes(routeProvider);
+
+			return new RouteBuilder(router.Routes);
+		} 
 	}
 }
