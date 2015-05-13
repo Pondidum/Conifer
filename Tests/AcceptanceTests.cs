d215575bb348fe8a9280a8721a236eef08b0bfee
andy <andy@andydote.co.uk>
2015-05-13 22:35:59 +0100

    tests: add acceptance tests for routing

---
+++ Tests/AcceptanceTests.cs
@@ -0,0 +1,53 @@
+﻿using System.Web.Http;
+using RestRouter;
+using Shouldly;
+using Tests.Controllers;
+using Xunit;
+
+namespace Tests
+{
+	public class AcceptanceTests
+	{
+		[Fact]
+		public void When_using_the_default_conventions()
+		{
+			var config = new HttpConfiguration();
+			var router = Router.Create(config, r =>
+			{
+				r.Add<HomeController>(null);	//no conventions applied to this route
+				r.Add<PersonController>();
+			});
+
+			router.ShouldSatisfyAllConditions(
+				() => router.RouteFor<HomeController>(c => c.Get()).ShouldBe(""),
+				() => router.RouteFor<PersonController>(c => c.Get()).ShouldBe("Person/Get"),
+				() => router.RouteFor<PersonController>(c => c.GetByID(123)).ShouldBe("Person/GetByID/123")
+			);
+			
+		}
+	}
+
+	namespace Controllers
+	{
+		public class HomeController : ApiController
+		{
+			public string Get()
+			{
+				return "Home.Get";
+			}
+		}
+
+		public class PersonController : ApiController
+		{
+			public string Get()
+			{
+				return "Person.Get";
+			}
+
+			public string GetByID(int id)
+			{
+				return "Person.Get(" + id + ")";
+			}
+		}
+	}
+}
