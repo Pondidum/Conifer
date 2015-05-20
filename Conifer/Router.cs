3181dc4ee9d77faad0c187706d94c9b77cb9a116
andy <andy@andydote.co.uk>
2015-05-20 22:52:54 +0100

    router: add xml comments

--- Conifer/Router.cs
+++ Conifer/Router.cs
@@ -5,6 +5,11 @@ namespace Conifer
 {
 	public class Router
 	{
+		/// <summary>
+		/// Creates a strong-typed router for routing and linking actions
+		/// </summary>
+		/// <param name="httpConfiguration">The HttpConfiguration to attach the routes to.</param>
+		/// <param name="configure">Expression to register controllers and setup conventions</param>
 		public static RouteBuilder Create(HttpConfiguration httpConfiguration, Action<RouterConfigurationExpression> configure)
 		{
 			var router = new ConventionalRouter();
--- Conifer/RouterConfigurationExpression.cs
+++ Conifer/RouterConfigurationExpression.cs
@@ -15,17 +15,27 @@ namespace Conifer
 			_router = router;
 		}
 
+		/// <summary>
+		/// Setup the default convetions to create routes
+		/// </summary>
 		public void DefaultConventionsAre(IEnumerable<IRouteConvention> conventions)
 		{
 			_defaultConventions.Clear();
 			_defaultConventions.AddRange(conventions);
 		}
 
+		/// <summary>
+		/// Create routes for all the applicable methods in the controller
+		/// </summary>
 		public void Add<TController>() where TController : IHttpController
 		{
 			Add<TController>(_defaultConventions);
 		}
 
+		/// <summary>
+		/// Create routes for all the applicable methods in the controller
+		/// </summary>
+		/// <param name="conventions">Override the default conventions with a custom set</param>
 		public void Add<TController>(IEnumerable<IRouteConvention> conventions) where TController : IHttpController
 		{
 			if (conventions == null) conventions = Enumerable.Empty<IRouteConvention>();
