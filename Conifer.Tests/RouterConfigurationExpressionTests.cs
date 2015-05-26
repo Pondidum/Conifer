d7418111740255f8ae485d80d3989e5c1195164d
andy <andy@andydote.co.uk>
2015-05-26 20:23:38 +0100

    routerConfigurationExpressionTests: add tests for AddRoute<>

--- Conifer.Tests/RouterConfigurationExpressionTests.cs
+++ Conifer.Tests/RouterConfigurationExpressionTests.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
 using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
@@ -54,11 +56,49 @@ namespace Conifer.Tests
 			_router.Received().AddRoutes<Controller>(Arg.Do<List<IRouteConvention>>(x => x.ShouldBe(new[] { new ControllerNameRouteConvention() })));
 		}
 
+		[Fact]
+		public void When_adding_a_single_method_and_no_default_conventions_are_specified()
+		{
+			_expression.Add<Controller>(c => c.Test());
+
+			_router.Received().AddRoute<Controller>(Arg.Any<Expression<Action<Controller>>>(), Arg.Do<List<IRouteConvention>>(x => x.ShouldBe(Default.Conventions)));
+		}
+
+		[Fact]
+		public void When_adding_a_single_method_and_no_default_conventions_are_specified_and_individual_are_used()
+		{
+			_expression.Add<Controller>(c => c.Test(), new[] { new ControllerNameRouteConvention() });
+
+			_router.Received().AddRoute<Controller>(Arg.Any<Expression<Action<Controller>>>(), Arg.Do<List<IRouteConvention>>(x => x.ShouldBe(new[] { new ControllerNameRouteConvention() })));
+		}
+
+		[Fact]
+		public void When_adding_a_single_method_and_no_default_conventions_are_specified_and_null_is_specified()
+		{
+			_expression.Add<Controller>(c => c.Test(), null);
+
+			_router.Received().AddRoute<Controller>(Arg.Any<Expression<Action<Controller>>>(), Arg.Do<List<IRouteConvention>>(x => x.ShouldBeEmpty()));
+		}
+
+		[Fact]
+		public void When_adding_a_single_method_and_default_conventions_are_specified()
+		{
+			_expression.DefaultConventionsAre(new[] { new ControllerNameRouteConvention() });
+			_expression.Add<Controller>(c => c.Test());
+
+			_router.Received().AddRoute<Controller>(Arg.Any<Expression<Action<Controller>>>(), Arg.Do<List<IRouteConvention>>(x => x.ShouldBe(new[] { new ControllerNameRouteConvention() })));
+		}
+
 		private class Controller : IHttpController
 		{
 			public Task<HttpResponseMessage> ExecuteAsync(HttpControllerContext controllerContext, CancellationToken cancellationToken)
 			{
 				throw new System.NotImplementedException();
+			}
+
+			public string Test()
+			{
+				return string.Empty;
 			}
 		}
 	}
