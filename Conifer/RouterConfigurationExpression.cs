e6607fb1f00b1a446fcfb4adc869c329a25006d0
andy <andy@andydote.co.uk>
2015-05-26 19:45:16 +0100

    routerConfigurationExpression: update xml comments

--- Conifer/RouterConfigurationExpression.cs
+++ Conifer/RouterConfigurationExpression.cs
@@ -17,27 +17,21 @@ namespace Conifer
 			_router = router;
 		}
 
-		/// <summary>
-		/// Setup the default convetions to create routes
-		/// </summary>
+		/// <summary>Setup the default convetions to create routes</summary>
 		public void DefaultConventionsAre(IEnumerable<IRouteConvention> conventions)
 		{
 			_defaultConventions.Clear();
 			_defaultConventions.AddRange(conventions);
 		}
 
-		/// <summary>
-		/// Create routes for all the applicable methods in the controller
-		/// </summary>
+		/// <summary>Create routes for all the applicable methods in the controller</summary>
 		public void AddAll<TController>()
 			where TController : IHttpController
 		{
 			AddAll<TController>(_defaultConventions);
 		}
 
-		/// <summary>
-		/// Create routes for all the applicable methods in the controller
-		/// </summary>
+		/// <summary>Create routes for all the applicable methods in the controller</summary>
 		/// <param name="conventions">Override the default conventions with a custom set</param>
 		public void AddAll<TController>(IEnumerable<IRouteConvention> conventions)
 			where TController : IHttpController
@@ -47,8 +41,7 @@ namespace Conifer
 			_router.AddRoutes<TController>(conventions.ToList());
 		}
 
-		/// <summary>
-		/// Creates a route for the specified method in the controller
+		/// <summary>Creates a route for the specified method in the controller
 		/// </summary>
 		public void Add<TController>(Expression<Action<TController>> expression)
 			where TController : IHttpController
@@ -56,9 +49,8 @@ namespace Conifer
 			Add(expression, _defaultConventions);
 		}
 
-		/// <summary>
-		/// Creates a route for the specified method in the controller
-		/// </summary>
+		/// <summary>Creates a route for the specified method in the controller</summary>
+		/// <param name="expression">The action to create a route for</param>
 		/// <param name="conventions">Override the default conventions with a custom set</param>
 		public void Add<TController>(Expression<Action<TController>> expression, IEnumerable<IRouteConvention> conventions)
 			where TController : IHttpController
