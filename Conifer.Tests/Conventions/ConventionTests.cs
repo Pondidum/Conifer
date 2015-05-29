f2677ea6a5902661c8979181423b4d398390b3d7
andy <andy@andydote.co.uk>
2015-05-29 17:42:56 +0100

    rawRouteConventionTests: refactor tests to use conventionTests base class

--- Conifer.Tests/Conventions/ConventionTests.cs
+++ Conifer.Tests/Conventions/ConventionTests.cs
@@ -13,7 +13,13 @@ namespace Conifer.Tests.Conventions
 		public HashSet<HttpMethod> SupportedMethods { get; private set; }
 
 		public Func<IRouteConvention> Convention { get; set; }
+		public Action<TypedRouteBuilder> PreConfigure { get; set; }
 
+		public ConventionTests()
+		{
+			PreConfigure = t => { };
+		}
+ 
 		protected void ExecuteConventionOn<T>() where T : IHttpController
 		{
 			ExecuteConventionOn<T>(c => c.ToString());
@@ -23,6 +29,8 @@ namespace Conifer.Tests.Conventions
 		{
 			var method = (expression.Body as MethodCallExpression).Method;
 			var template = new TypedRouteBuilder(typeof(T), method);
+			
+			PreConfigure.Invoke(template);
 
 			var convention = Convention.Invoke();
 			convention.Execute(template);
--- Conifer.Tests/Conventions/RawRouteConventionTests.cs
+++ Conifer.Tests/Conventions/RawRouteConventionTests.cs
@@ -1,57 +1,47 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+using System.Web.Http;
 using Conifer.Conventions;
 using Shouldly;
 using Xunit;
 
 namespace Conifer.Tests.Conventions
 {
-	public class RawRouteConventionTests
+	public class RawRouteConventionTests : ConventionTests
 	{
-		private List<string> RunTest(string methodName)
+		public RawRouteConventionTests()
 		{
-			return RunTest(methodName, Enumerable.Empty<RoutePart>());
-		}
-
-		private List<string> RunTest(string methodName, IEnumerable<RoutePart> initialParts)
-		{
-			var method = GetType().GetMethod(methodName, BindingFlags.Instance | BindingFlags.NonPublic);
-
-			var template = new TypedRouteBuilder(typeof(Controller), method);
-			template.Parts.AddRange(initialParts);
-
-			var convention = new RawRouteConvention();
-			convention.Execute(template);
-
-			return template
-				.Parts
-				.Select(p => p.Value)
-				.ToList();
+			Convention = () => new RawRouteConvention();
 		}
 
 		[Fact]
 		public void When_the_action_ends_in_raw()
 		{
-			RunTest("GetDocumentRaw").Single().ShouldBe("raw");
+			ExecuteConventionOn<Controller>(c => c.GetDocumentRaw());
+			Route.ShouldBe("/raw");
 		}
 
 		[Fact]
 		public void When_the_action_doesnt_end_in_raw()
 		{
-			RunTest("GetDocument").ShouldBeEmpty();
+			ExecuteConventionOn<Controller>(c => c.GetDocument());
+			Route.ShouldBeEmpty();
 		}
 
 		[Fact]
-		public void When_there_is_an_action_ending_in_raw()
+		public void When_there_is_an_action_in_the_route_ending_in_raw()
 		{
-			RunTest("GetDocumentRaw", new[] { new ActionRoutePart { Value = "GetDocumentRaw" }  })
-				.First().ShouldBe("GetDocument");
+			PreConfigure = template => template.Parts.Add(new ActionRoutePart {Value = "GetDocumentRaw"});
+
+			ExecuteConventionOn<Controller>(c => c.GetDocumentRaw());
+			Route.ShouldBe("/GetDocument/raw");
 		}
 
-		private class Controller { }
-
-		private void GetDocumentRaw() { }
-		private void GetDocument() { }
+		private class Controller : ApiController
+		{
+			public void GetDocumentRaw() { }
+			public void GetDocument() { }
+		}
 	}
 }
